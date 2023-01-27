e03090ca0d48188a0d8c8d35a72ac1aecd14df20
natanlopes <natan11918@gmail.com>
2023-01-27 09:49:33 -0300

    add
    import

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
