d487548a9e8abcf7ca35f1bc44022aeaa81f7bca
natanlopes <natan11918@gmail.com>
2023-01-30 11:33:53 -0300

    metado de requisição

--- cursos/views.py
+++ cursos/views.py
@@ -1,23 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-
 class CursoAPIView(APIView):
-    """ api de cursos
     """
-    def get(request):
+    APi de curso
+    """
+    def get(self,request):
         cursos = Curso.objects.all()
-        serilizer = CursoSerializer(cursos, many=True)
+        serilizer = CursoSerializer(cursos,many=True)
         return Response(serilizer.data)
-
 
 class AvaliacaoAPIView(APIView):
-    """ api de avaliacao
     """
-
-    def get(request):
+    APi de Avaliacao
+    """
+    def get(self,request):
         avaliacoes = Avaliacao.objects.all()
-        serilizer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serilizer.data)
+        serializer = AvaliacaoSerializer(avaliacoes,many=True)
+        return Response(serializer.data)
