485f6bee3d298b56d5f588752ed029006cb5a112
natanlopes <natan11918@gmail.com>
2023-01-30 17:01:37 -0300

    get e post

--- cursos/views.py
+++ cursos/views.py
@@ -1,23 +1,6 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from .models import Curso,Avaliacao
+from .serializers import   CursoSerializer,AvaliacaoSerializer
 
-from .models import Curso, Avaliacao
-from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(APIView):
-    """
-    APi de curso
-    """
-    def get(self,request):
-        cursos = Curso.objects.all()
-        serilizer = CursoSerializer(cursos,many=True)
-        return Response(serilizer.data)
-
-class AvaliacaoAPIView(APIView):
-    """
-    APi de Avaliacao
-    """
-    def get(self,request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes,many=True)
-        return Response(serializer.data)
+class Curso
