5a59fdd26409762e553c9d027559d2eb1057ebe0
Natan Lopes <natan1918@gmail.com>
2023-03-11 11:42:16 -0300

    add delete e put
    adcionando o delete e o put e o editar no crud

--- cursos/views.py
+++ cursos/views.py
@@ -3,10 +3,17 @@ from .models import Curso,Avaliacao
 from .serializers import   CursoSerializer,AvaliacaoSerializer
 
 
-class CursoAPIView(generics.ListCreateAPIView):
+class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-class AvaliacaoAPIView (generics.ListCreateAPIView):
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+class AvaliacaoesAPIView (generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class AvaliacaoAPIView (generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
