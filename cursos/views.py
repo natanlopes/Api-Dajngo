f6f4ff95d3d77c6c84bd9f6c89c3c18f0e481f33
Natan Lopes <natan1918@gmail.com>
2023-03-11 10:51:56 -0300

    add view
    adcionando views api

--- cursos/views.py
+++ cursos/views.py
@@ -3,4 +3,10 @@ from .models import Curso,Avaliacao
 from .serializers import   CursoSerializer,AvaliacaoSerializer
 
 
-class Curso
+class CursoAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class AvaliacaoAPIView (generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
