667d5dc27e4d079b5e989d4e948c4db802aba5ae
Natan Lopes <natan1918@gmail.com>
2023-04-30 10:18:04 -0300

    Testando o método GET
    testando o endpoint esta correto
    testando a quantidade de registros
    # #testando se o titulo do primeiro curso esta correto

--- test_get.py
+++ test_get.py
@@ -9,4 +9,13 @@ url_base_avaliacoes= 'http://127.0.0.1:8000/api/v2/cursos/'
 
 resultado = requests.get(url=url_base_cursos,headers=headers)
 
-print(resultado.json())
+# print(resultado.json())
+# #testando o endpoint esta correto
+#
+# assert  resultado.status_code == 200
+# #testando a quantidade de registros
+assert  resultado.json()['count'] == 6
+#
+# #testando se o titulo do primeiro curso esta correto
+
+assert resultado.json()['results'][0] ['titulo'] == 'Python API'
