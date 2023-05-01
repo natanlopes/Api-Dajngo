bbe9a410ff4bd6927c49af97543309e0f72b1a1d
Natan Lopes <natan1918@gmail.com>
2023-05-01 16:07:08 -0300

    Testando o método PUT
    Este código em Python faz uma requisição PUT para atualizar as informações de um curso em uma API. O cabeçalho Authorization é utilizado para autenticar a requisição.
    
    A variável url_base_cursos armazena a URL base da API de cursos e a variável curso_atualizado armazena um dicionário com as informações atualizadas do curso.
    
    A função requests.put() é utilizada para fazer uma requisição HTTP PUT para a URL do curso com o ID 6, passando o cabeçalho de autorização e os dados atualizados do curso.
    
    As duas instruções assert são usadas para verificar se a atualização foi bem-sucedida. A primeira instrução verifica se o código de status HTTP retornado é 200 (OK) e a segunda instrução verifica se o título do curso retornado na resposta é igual ao título atualizado. Se a atualização não for bem-sucedida, as instruções assert levantarão uma exceção.

--- test_put.py
+++ test_put.py
@@ -5,3 +5,26 @@ headers = {'Authorization': 'Token beddec6af186cf36cc1e9fb6b5137b9ca140410a'}
 
 url_base_cursos = 'http://127.0.0.1:8000/api/v2/cursos/'
 url_base_avaliacoes = 'http://127.0.0.1:8000/api/v2/avaliacoes/'
+
+
+curso_atualizado ={
+    "titulo": "Novo Curso de Scrum 3",
+    "url":"http://www.geekuniversity.com.br/ncc3"
+}
+# #buscando se o curso com ID 6
+# curso = requests.get(url=f'{url_base_cursos}6/',headers=headers)
+# print(curso.json())
+
+resultado = requests.put(url=f'{url_base_cursos}6/', headers=headers, data=curso_atualizado)
+print(resultado.status_code)
+
+
+# resultado = requests.put(url=f'{url_base_cursos}6/',headers=headers,data=curso_atualizado)
+#
+#
+# #testando o codigo de status HTTP
+#
+# assert  resultado.status_code == 200
+#
+# #testando o titulo
+# assert resultado.json()['titulo']== curso_atualizado['titulo']
